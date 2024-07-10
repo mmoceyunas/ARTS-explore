9d335d8747315e7c02497fc9e57a4a35a2f0aa73
mmoceyunas <mmoceyunas@woodwellclimate.org>
2024-07-10 13:59:52 -0700

    Ggplots, subsetted by region.

--- arts-explore-scrap.R
+++ arts-explore-scrap.R
@@ -1,6 +1,27 @@
 #opening geojson file
-library(sf)
-arts.sf = st_read("ARTS_main_dataset.geojson")
-
 R.version.string
 update.packages(ask = FALSE, checkBuilt = TRUE)
+library(sf)
+arts_sf = st_read("ARTS_main_dataset.geojson")
+
+#explore mapping
+library(ggplot2)
+  #Region
+arts_sf_subset1 <- arts_sf[arts_sf$RegionName == "Banks Island, Inuvik Region, Canada", ]
+ggplot(data = arts_sf_subset1, aes(fill = TrainClass)) + 
+  geom_sf(col = "white") + 
+  coord_sf(datum = NULL) 
+
+arts_sf_subset3 <- arts_sf[arts_sf$RegionName == "Hot Weather Creek, Ellesmere, Canada", ]
+ggplot(data = arts_sf_subset3, aes(fill = TrainClass)) + 
+  geom_sf(col = "white") + 
+  coord_sf(datum = NULL)
+
+
+  #divide by TrainClass
+arts_sf_subset2 <- arts_sf[arts_sf$TrainClass == "Positive", ]
+ggplot(data = arts_sf_subset1) + 
+  geom_sf(col = "white") + 
+  coord_sf(datum = NULL) 
+
+#adding basemap?
