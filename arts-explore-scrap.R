49607f204ee2248f62a274a068a821c0878566a1
mmoceyunas <mmoceyunas@woodwellclimate.org>
2024-07-15 10:29:36 -0700

    Adding arctic basemap.

--- arts-explore-scrap.R
+++ arts-explore-scrap.R
@@ -3,15 +3,23 @@ R.version.string
 update.packages(ask = FALSE, checkBuilt = TRUE)
 library(sf)
 arts_sf = st_read("ARTS_main_dataset.geojson")
+st_crs(arts_sf)
+arts_sf
 
 #explore mapping
 library(ggplot2)
-  #Region
-arts_sf_subset1 <- arts_sf[arts_sf$RegionName == "Banks Island, Inuvik Region, Canada", ]
-ggplot(data = arts_sf_subset1, aes(fill = TrainClass)) + 
-  geom_sf(col = "white") + 
-  coord_sf(datum = NULL) 
+library(ggmap)
+library(png)
+library(grid)
 
+#Region = Banks Island
+arts_sf_subset1 <- arts_sf[arts_sf$RegionName == "Banks Island, Inuvik Region, Canada", ]
+test_layer <- ggplot(data = arts_sf_subset1, aes(fill = TrainClass)) + 
+  geom_sf(col = "white") + 
+  coord_sf(datum = NULL) +
+  theme_void()
+
+#Region = Hot Weather Creek
 arts_sf_subset3 <- arts_sf[arts_sf$RegionName == "Hot Weather Creek, Ellesmere, Canada", ]
 ggplot(data = arts_sf_subset3, aes(fill = TrainClass)) + 
   geom_sf(col = "white") + 
@@ -26,19 +34,22 @@ ggplot(data = arts_sf_subset1) +
   coord_sf(datum = NULL) 
 
 #adding basemap?
+site_map + geom_sf(data = arts_sf_subset1, aes(fill = TrainClass), col = "red")
+site_map + geom_sf(data = arts_sf_subset2, aes(fill = TrainClass), col = "red")
+site_map + geom_sf(data = arts_sf_subset3, aes(fill = TrainClass), col = "red")
+site_map + geom_sf(data = arts_sf, aes(fill = TrainClass), col = "red")
 
-install.packages(c("sp", "MASS", "reshape2","geojsonio","rgdal","downloader","maptools","dplyr","broom","stplanr", "ggplot2", "leaflet"))
-library(tidyverse)
-library(sp)
-library(MASS)
-library(reshape2)
-library(geojsonio)
-library(rgdal)
-library(downloader)
-library(maptools)
-library(broom) 
-library(stplanr)
-library(leaflet)
-library(sf)
+#spatial autocorrelation
 library(tmap)
+tm_shape(arts_sf_subset1) + tm_polygons(style="equal", border.col = "grey80", lwd = 0.5,
+                                col = "CreatorLab", palette="Greens") +
+  tm_legend(outside = TRUE, text.size = .8)  +
+  tm_layout(frame = FALSE)
 
+library(spdep)
+xy <- st_centroid(arts_sf_subset1) #defining neighboring based on distances to polygon centers
+s.dist <- dnearneigh(xy, 0, 1000) #define search radius
+s.dist[[1]] |> length()
+lw <- nb2listw(s.dist, style = "W", zero.policy = T) #computing weights
+lw$weights[[1]]
+MC <- moran.mc(arts_sf_subset1, lw, nsim = 599, zero.policy = T)
